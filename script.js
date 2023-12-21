989da392b717d57b01fa0c6122e03bbd123165c0
Alex Khmurovich <alexhmurovich@gmail.com>
2023-12-21 11:39:51 -0600

    Base layout in css/html

---
+++ script.js
@@ -0,0 +1,6 @@
+var buzzBtn = document.querySelector("#buzz-btn")
+var answerField = document.querySelector("#answer-field")
+
+buzzBtn.addEventListener("click", function() {
+    answerField.style.display = "block"
+})
