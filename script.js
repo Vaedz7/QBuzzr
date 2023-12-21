7f35a1d626990c94cf7740bd35514d14c38329cb
Alex Khmurovich <alexhmurovich@gmail.com>
2023-12-21 12:03:34 -0600

    Tossups and Bonuses subbuttons added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 var buzzBtn = document.querySelector("#buzz-btn")
 var answerField = document.querySelector("#answer-field")
+var questionsBtn = document.querySelector("#questions_btn")
+var sidebarBtnsSmall = document.querySelector(".sidebar-btns-small")
 
 buzzBtn.addEventListener("click", function() {
     answerField.style.display = "block"
 })
+
+questionsBtn.addEventListener("mouseover", function(){
+    sidebarBtnsSmall.style.display = "flex"
+})
+
+questionsBtn.addEventListener("mouseout", function(){
+    sidebarBtnsSmall.style.display = "none"
+})
+
