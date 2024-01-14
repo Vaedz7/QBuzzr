afc3f521f3db9863535fc65f173e3c8a89fe17e5
Vaedz <dev@vaedz.com>
2024-01-14 17:28:54 -0600

    Add React-based QBuzzr

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import Home from "./Home";
+import Play from "./Play";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/play" element={<Play />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
