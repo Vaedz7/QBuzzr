05719bf1fcacb7ac49ec8ee607e96a8a8bca0aad
Vaedz <dev@vaedz.com>
2024-02-26 13:01:44 -0600

    Fix closing elements

--- QBuzzr/src/Home.tsx
+++ QBuzzr/src/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1 className="text-9xl">Play.<br>Learn.<br>Enjoy.</h1>
+      <h1 className="text-9xl">Play.<br />Learn.<br />Enjoy.</h1>
     </>
   )
 }
