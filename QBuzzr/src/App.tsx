9869ca68b7d1645744ec52717734f76adc462070
Vaedz <dev@vaedz.com>
2024-02-26 11:34:04 -0600

    Remove unused imports

--- QBuzzr/src/App.tsx
+++ QBuzzr/src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
 import { Button } from "@/components/ui/button"
