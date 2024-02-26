6042dfd912a7759fa97f1a2f407613fe4b295cfc
Vaedz <dev@vaedz.com>
2024-02-26 13:00:39 -0600

    Add text screen

---
+++ QBuzzr/src/Home.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Navbar from './Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-9xl">Play.<br>Learn.<br>Enjoy.</h1>
+    </>
+  )
+}
