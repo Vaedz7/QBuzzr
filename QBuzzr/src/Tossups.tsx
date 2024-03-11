df3a365b0e5bdfe974812853a5e056ed45206ce2
Alex Khmurovich <alexhmurovich@gmail.com>
2024-03-11 12:04:31 -0500

    Add a favicon, changed the home layout

--- QBuzzr/src/Tossups.tsx
+++ QBuzzr/src/Tossups.tsx
@@ -25,9 +25,9 @@ export default function Home() {
   return (
     <>
         <Navbar/>
-        <div className='flex flex-col items-center'>
+        <div className='flex flex-col items-center mt-4'>
             <TossupCard text="It's not Brazil, but this region's Luquillo Mountains are home to a namesake Amazon parrot. A mix of islands and cays including Culebra and Vieques are located off the eastern coast of this island. An endemic species of frog known as coqui are located in this island's El Yunque National Forest. This island's central region is home to (*) Arecibo Observatory, one of the world's largest radio telescopes. (#) The successive 2017 natural disasters of Hurricane Irma and Maria damaged much of the infrastructure on this island. For ten points name this island territory of the United States, with capital San Juan." answer="ANSWER: Commonwealth of Puerto Rico [accept Boriken]" category='Geography'/>
-            <div className='flex flex-row justify-evenly w-full mt-4 gap-2'>
+            <div className='flex flex-row justify-evenly w-full mt-2 gap-2'>
                 <Input placeholder='Enter answer' className='w-full'/>
                 <div className='flex flex-row gap-2'>
                     <Button>Buzz</Button>
@@ -39,6 +39,25 @@ export default function Home() {
                 <DropdownMenuTrigger>Difficulties</DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel>Difficulties</DropdownMenuLabel>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuCheckboxItem checked={true}>MS</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Easy HS</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Regular HS</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Hard HS</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>National MS</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Easy COLL</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Regular COLL</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Hard COLL</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>National COLL</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem checked={true}>Open</DropdownMenuCheckboxItem>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuCheckboxItem checked={true}>Select All</DropdownMenuCheckboxItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger>Categories</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Categories</DropdownMenuLabel>
                     <DropdownMenuSeparator/>
                     <DropdownMenuCheckboxItem checked={true}>MS</DropdownMenuCheckboxItem>
                     <DropdownMenuCheckboxItem checked={true}>Easy HS</DropdownMenuCheckboxItem>
