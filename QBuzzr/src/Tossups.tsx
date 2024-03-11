a70d585b26a5845063cf55089a037f03126cecd8
Alex Khmurovich <alexhmurovich@gmail.com>
2024-03-11 17:43:17 -0500

    added position sticky to the enter field

--- QBuzzr/src/Tossups.tsx
+++ QBuzzr/src/Tossups.tsx
@@ -106,7 +106,7 @@ export default function Home() {
             <ResizablePanel className='p-2'>
                 <div className='flex flex-col items-center'>
                     <TossupCard text="It's not Brazil, but this region's Luquillo Mountains are home to a namesake Amazon parrot. A mix of islands and cays including Culebra and Vieques are located off the eastern coast of this island. An endemic species of frog known as coqui are located in this island's El Yunque National Forest. This island's central region is home to (*) Arecibo Observatory, one of the world's largest radio telescopes. (#) The successive 2017 natural disasters of Hurricane Irma and Maria damaged much of the infrastructure on this island. For ten points name this island territory of the United States, with capital San Juan." answer="ANSWER: Commonwealth of Puerto Rico [accept Boriken]" category='Geography'/>
-                    <div className='flex flex-row justify-evenly w-full mt-2 gap-2'>
+                    <div className='flex flex-row justify-evenly w-full mt-2 gap-2 sticky bottom-0'>
                         <Input placeholder='Enter answer' className='w-full'/>
                         <div className='flex flex-row gap-2'>
                             <Button>Buzz</Button>
