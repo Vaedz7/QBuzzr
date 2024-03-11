323906c3e930bb4e1f4dd21c85f9baae7c83df6b
Alex Khmurovich <alexhmurovich@gmail.com>
2024-03-11 11:01:57 -0500

    added a basic enter panel with buttons

--- QBuzzr/src/Tossups.tsx
+++ QBuzzr/src/Tossups.tsx
@@ -3,6 +3,8 @@ import './App.css'
 // import * as React from "react"
 import Navbar from './Navbar'
 import TossupCard from './TossupCard';
+import { Input } from "@/components/ui/input"
+import { Button } from './components/ui/button';
 
 
 
@@ -10,7 +12,17 @@ export default function Home() {
   return (
     <>
         <Navbar/>
-        <TossupCard text="It's not Brazil, but this region's Luquillo Mountains are home to a namesake Amazon parrot. A mix of islands and cays including Culebra and Vieques are located off the eastern coast of this island. An endemic species of frog known as coqui are located in this island's El Yunque National Forest. This island's central region is home to (*) Arecibo Observatory, one of the world's largest radio telescopes. (#) The successive 2017 natural disasters of Hurricane Irma and Maria damaged much of the infrastructure on this island. For ten points name this island territory of the United States, with capital San Juan." answer="ANSWER: Commonwealth of Puerto Rico [accept Boriken]" category='Geography'/>
+        <div className='flex flex-col items-center'>
+            <TossupCard text="It's not Brazil, but this region's Luquillo Mountains are home to a namesake Amazon parrot. A mix of islands and cays including Culebra and Vieques are located off the eastern coast of this island. An endemic species of frog known as coqui are located in this island's El Yunque National Forest. This island's central region is home to (*) Arecibo Observatory, one of the world's largest radio telescopes. (#) The successive 2017 natural disasters of Hurricane Irma and Maria damaged much of the infrastructure on this island. For ten points name this island territory of the United States, with capital San Juan." answer="ANSWER: Commonwealth of Puerto Rico [accept Boriken]" category='Geography'/>
+            <div className='flex flex-row justify-evenly w-full mt-4'>
+                <Input placeholder='Enter answer' className='w-full'/>
+                <div className='flex flex-row gap-2'>
+                    <Button>Buzz</Button>
+                    <Button variant="secondary">Next</Button>
+                    <Button variant="secondary">Stop</Button>
+                </div>
+            </div>
+        </div>
     </>
   );
 }
