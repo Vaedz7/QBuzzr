8b89c2d42b87907e6e166c30d6f3ab1ea0100999
Vaedz <dev@vaedz.com>
2024-03-12 17:30:24 -0500

    try removing props declaration?

--- QBuzzr/src/components/ui/calendar.tsx
+++ QBuzzr/src/components/ui/calendar.tsx
@@ -11,7 +11,7 @@ function Calendar({
   className,
   classNames,
   showOutsideDays = true,
-  ...props,
+  //...props
 }: CalendarProps) {
   return (
     <DayPicker
@@ -51,11 +51,13 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
+      /*
       components={{
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
+      */
     />
   )
 }
