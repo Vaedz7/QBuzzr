0e8ec0f30a8b3efbec8c2d05664eed38cbf3028c
Alex Khmurovich <alexhmurovich@gmail.com>
2024-03-13 21:21:54 -0500

    Optimization for dark/light mode

--- QBuzzr/src/App.tsx
+++ QBuzzr/src/App.tsx
@@ -1,17 +1,20 @@
-import './App.css'
-import Home from './Home'
-import Tossups from "./Tossups"
+import "./App.css";
+import Home from "./Home";
+import Tossups from "./Tossups";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tossups" element={<Tossups />} />
-        </Routes>
-      </Router>
-    </>
-  )
+   return (
+      <>
+         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Router>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/tossups" element={<Tossups />} />
+               </Routes>
+            </Router>
+         </ThemeProvider>
+      </>
+   );
 }
