414b2a79ae9dfda0040fe2fba181a69fddc140f5
Vaed <dev@vaedz.com>
2024-03-26 12:46:16 -0500

    Remove comment braces

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,8 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
-      {<ClerkProvider publishableKey={PUBLISHABLE_KEY}>}
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
          <Home />
-      {</ClerkProvider>}
+      </ClerkProvider>
    </React.StrictMode>
 );
