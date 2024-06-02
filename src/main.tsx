c0baf30e0d2f015ed40489ec2e35ec261d99ecc1
Vaed <dev@vaedz.com>
2024-06-02 15:27:54 -0500

    Add backend_key var

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const BACKEND_KEY = import.meta.env.REACT_APP_KEY;
 
 if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key");
